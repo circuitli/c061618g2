689d7eed52a4899c71477cb23ae10337e503da51
circuitli <dev@circuili.ch>
2026-09-13 21:22:23 +0200

    Doubled letter

--- openlane/c061618g2/pdn_cfg.tcl
+++ openlane/c061618g2/pdn_cfg.tcl
@@ -72,7 +72,7 @@ add_pdn_stripe -grid stdcell_grid \
                -layer $::env(PDN_HORIZONTAL_LAYER) \
                -width $::env(FP_PDN_HWIDTH) \
                -pitch $::env(FP_PDN_HPITCH) \
-               -offset [expr {$::env(FP_PDN_HHPITCH) / 2.0}] \
+               -offset [expr {$::env(FP_PDN_HPITCH) / 2.0}] \
                -spacing $::env(FP_PDN_HSPACING) \
                -nets "$::env(GND_NET) $::env(VDD_NET)" \
                -extend_to_core_ring
