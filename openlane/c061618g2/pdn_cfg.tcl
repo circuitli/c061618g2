b09ecfdbedf5a2fabd4dfb0d9d4c2b10e213661e
circuitli <dev@circuili.ch>
2026-09-13 20:18:38 +0200

    Redundant hop

--- openlane/c061618g2/pdn_cfg.tcl
+++ openlane/c061618g2/pdn_cfg.tcl
@@ -69,7 +69,7 @@ add_pdn_stripe -grid stdcell_grid \
                -extend_to_boundary
 
 # 4. Connect the layers cleanly together via native layer connectivity strings
-add_pdn_connect -grid stdcell_grid -layers "$::env(PDN_HORIZONTAL_LAYER) Metal2"
+#add_pdn_connect -grid stdcell_grid -layers "$::env(PDN_HORIZONTAL_LAYER) Metal2"
 add_pdn_connect -grid stdcell_grid -layers "$::env(PDN_VERTICAL_LAYER) $::env(PDN_HORIZONTAL_LAYER)"
 
 # Compliance macro integration grid
