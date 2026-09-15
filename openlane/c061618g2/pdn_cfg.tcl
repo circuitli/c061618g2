994682818b60555cd0965374ffd951a8f894eb5f
circuitli <dev@circuili.ch>
2026-09-15 11:53:07 +0200

    Do not extend

--- openlane/c061618g2/pdn_cfg.tcl
+++ openlane/c061618g2/pdn_cfg.tcl
@@ -94,8 +94,7 @@ add_pdn_stripe -grid stdcell_grid \
                -width $::env(PDN_VWIDTH) \
                -pitch $::env(PDN_VPITCH) \
                -offset $::env(PDN_VOFFSET) \
-               -spacing $::env(PDN_VSPACING) \
-               -extend_to_boundary
+               -spacing $::env(PDN_VSPACING) 
 
 # 3. Horizontal Mesh Power Landing Pads -> EXTEND_TO_BOUNDARY
 add_pdn_stripe -grid stdcell_grid \
@@ -104,8 +103,7 @@ add_pdn_stripe -grid stdcell_grid \
                -pitch $::env(PDN_HPITCH) \
                -offset $::env(PDN_HOFFSET) \
                -spacing $::env(PDN_HSPACING) \
-               -nets "$::env(VDD_NET) $::env(GND_NET)" \
-               -extend_to_boundary
+               -nets "$::env(VDD_NET) $::env(GND_NET)" 
 
 # 4. Connect the layers cleanly together via native layer connectivity strings
 #add_pdn_connect -grid stdcell_grid -layers "$::env(PDN_HORIZONTAL_LAYER) Metal2"
